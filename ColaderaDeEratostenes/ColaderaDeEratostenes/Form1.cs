7bee87088decfa289081bf00b9360efbed5a1290
Shernandezglez <32920669+Shernandezglez@users.noreply.github.com>
2018-03-23 20:37:58 -0600

    Add files via upload

---
+++ ColaderaDeEratostenes/ColaderaDeEratostenes/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColaderaDeEratostenes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        MetodoDeColadera mtc;
+
+        private void btnEmpezar_Click(object sender, EventArgs e)
+        {
+            mtc = new MetodoDeColadera(100);
+
+            mtc.inicializarArreglo();
+            mtc.buscarMultiplos();
+            
+            textBox1.Text += mtc.mostrar().ToString();
+        }
+
+
+
+       
+    }
+}
---
+++ ColaderaDeEratostenes/ColaderaDeEratostenes/MetodoDeColadera.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColaderaDeEratostenes
+{
+    class MetodoDeColadera
+    {
+        private bool[] _num;
+        private int[] _index;
+
+        public MetodoDeColadera(int lim)
+        {
+            _num = new bool[lim];
+            _index = new int[lim];
+        }
+
+        public void inicializarArreglo()
+        {
+            for (int i = 0; i < _num.Length -1; i++)
+            {
+                _num[i] = true;
+                _index[i] = i + 1;
+            }
+        }
+
+        public void buscarMultiplos()
+        {
+            for(int i = 2; i< _index.Length; i++)
+            {
+                if(_num[i] == true)
+                {
+                   
+                    if (_index[i + 1] % 2 == 1 || _index[i] % 3 == 0 || _index[i] % 5 == 0 || _index[i] % 7 == 0)
+                    {
+                        _num[i] = false;
+                    }
+                    else
+                    {
+                        _num[i] = true;
+                    }
+                }
+            }
+        }
+
+     
+
+        public string mostrar()
+        {
+           
+            string cosa = "";
+            for (int i = 0; i < _num.Length; i++)
+            {
+                if(_num[i] == true)
+                {
+                    cosa += _index[i].ToString() + ", ";
+                }
+                else
+                {
+                    cosa += "";
+                }
+                
+            }
+
+            return cosa;
+        }
+
+
+
+
+
+
+
+
+    }
+}
